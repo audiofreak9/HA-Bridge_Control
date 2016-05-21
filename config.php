c36c958a6e397eb69a4e4f8673c3fd8ed2673d13
audiofreak9 <audiofreak9@gmail.com>
2016-05-20 23:36:14 -0400

    port
    port

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $port = 8080;
 //Get Server Name (IP)
 $SN = $_SERVER['SERVER_NAME'];
 //Get the devices from the bridge, put them into an array
-$ha_devices = json_decode(file_get_contents("http://$SN:8080/api/devices"), true);
+$ha_devices = json_decode(file_get_contents("http://$SN:" . $port . "/api/devices"), true);
 $deviceTypes = array();
 //Get each device type into an array
 foreach ($ha_devices as $ha_device) $deviceTypes[] = $ha_device["deviceType"];
