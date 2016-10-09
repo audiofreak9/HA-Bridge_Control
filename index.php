437e1028a838c03e20511a5e2c1af172b6377f2a
audiofreak9 <audiofreak9@gmail.com>
2016-10-08 20:26:26 -0400

    Javascript clean up
    Javascript clean up

--- index.php
+++ index.php
@@ -63,47 +63,9 @@ for($x = 0; $x <= count($ha_devices) - 1; $x++) {
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js?ver=CDN"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <script>
-$(".btn").click(function() {
-  var dev_id = $(this).closest("form").attr('id');
-  var dev_val = $(this).val();
-  var dev_per = $('#ps' + dev_id).attr('value');
-  var data = '{"on":';
-  if(dev_val == "off") {
-    percent = 0;
-    data += 'false';
-  }else{
-    percent = dev_per;
-    data += 'true';
-    if(dev_per < 100) {
-      data += ', "bri":' + Math.round(2.55 * dev_per);
-    }
-  }
-  data += '}';
-  $.ajax({
-    type: 'POST',
-    dataType: 'json',
-    url: 'http://<?php echo $SN; ?>:<?php echo $port; ?>/api/c/lights/' + dev_id + '/state',
-    headers: {"X-HTTP-Method-Override": "PUT"},
-    data: data,
-    success : updateProgress(percent, dev_id)
-  });
-  return false;
-});
-$('.sl').on("change", function() {
-    var my_id = $(this).closest("form").attr('id');
-    var level = $(this).val();
-    $('#ps' + my_id).val(level);
-    $('#l' + my_id).html(level + '%').fadeIn( 400 ).delay( 800 ).fadeOut( 400 ); 
-});
-function updateProgress(percent, dev_id){
-    if(percent > 100) percent = 100;
-    $('#prog' + dev_id).css('width', percent+'%');
-    $('#prog' + dev_id).html(percent+'%');
-    if(percent == 0) {
-        $('#sl' + dev_id).prop("value", "100");
-        $('#ps' + dev_id).val('100');
-    }
-}
+var SN = '<?php echo $SN; ?>';
+var port = '<?php echo $port; ?>';
 </script>
+<script src="js/controlscript.js"></script>
 </body>
 </html>
---
+++ js/controlscript.js
@@ -0,0 +1,41 @@
+$(".btn").click(function() {
+  var dev_id = $(this).closest("form").attr('id');
+  var dev_val = $(this).val();
+  var dev_per = $('#ps' + dev_id).attr('value');
+  var data = '{"on":';
+  if(dev_val == "off") {
+    percent = 0;
+    data += 'false';
+  }else{
+    percent = dev_per;
+    data += 'true';
+    if(dev_per < 100) {
+      data += ', "bri":' + Math.round(2.55 * dev_per);
+    }
+  }
+  data += '}';
+  $.ajax({
+    type: 'POST',
+    dataType: 'json',
+    url: 'http://' + SN + ':' + port + '/api/c/lights/' + dev_id + '/state',
+    headers: {"X-HTTP-Method-Override": "PUT"},
+    data: data,
+    success : updateProgress(percent, dev_id)
+  });
+  return false;
+});
+$('.sl').on("change", function() {
+    var my_id = $(this).closest("form").attr('id');
+    var level = $(this).val();
+    $('#ps' + my_id).val(level);
+    $('#l' + my_id).html(level + '%').fadeIn( 400 ).delay( 800 ).fadeOut( 400 ); 
+});
+function updateProgress(percent, dev_id){
+    if(percent > 100) percent = 100;
+    $('#prog' + dev_id).css('width', percent+'%');
+    $('#prog' + dev_id).html(percent+'%');
+    if(percent == 0) {
+        $('#sl' + dev_id).prop("value", "100");
+        $('#ps' + dev_id).val('100');
+    }
+}
