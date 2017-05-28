4dd7ddf377c235acc405ce0d987f7285b2dc3996
audiofreak9 <audiofreak9@gmail.com>
2017-05-28 17:52:32 -0400

    Update controlscript.js

--- js/controlscript.js
+++ js/controlscript.js
@@ -1,61 +1,3 @@
-$(".act").click(function() {
-  var dev_id = $(this).closest("form").attr('id');
-  var dev_val = $(this).val();
-  var dev_per = $('#ps' + dev_id).attr('value');
-  var data = '{"on":';
-  if(dev_val == "off") {
-    percent = 0;
-    data += 'false';
-  }else{
-    percent = dev_per;
-    data += 'true';
-    if(dev_per < 100) {
-      data += ', "bri":' + Math.round(2.55 * dev_per);
-    }
-  }
-  data += '}';
-  $.ajax({
-    type: 'POST',
-    dataType: 'json',
-    url: 'http://' + SN + ':' + port + '/api/c/lights/' + dev_id + '/state',
-    headers: {"X-HTTP-Method-Override": "PUT"},
-    data: data,
-    success : updateProgress(percent, dev_id, dev_val)
-  });
-  return false;
-});
-$(".upd").click(function() {
-  var dev_id = $(this).closest("form").attr('id');
-  var dev_val = $(this).val();
-  var dev_per = $('#ps' + dev_id).attr('value');
-  var data = '{"on":';
-  if(dev_val == "off") {
-    percent = 0;
-    data += 'false';
-  }else{
-    percent = dev_per;
-    data += 'true';
-    if(dev_per < 100) {
-      data += ', "bri":' + Math.round(2.55 * dev_per);
-    }
-  }
-  data += '}';
-  $.ajax({
-    type: 'POST',
-    dataType: 'json',
-    url: 'http://' + SN + ':' + port + '/api/c/lights/' + dev_id + '/bridgeupdatestate',
-    headers: {"X-HTTP-Method-Override": "PUT"},
-    data: data,
-    success : updateProgress(percent, dev_id, dev_val)
-  });
-  return false;
-});
-$('.sl').on("change", function() {
-    var my_id = $(this).closest("form").attr('id');
-    var level = $(this).val();
-    $('#ps' + my_id).val(level);
-    $('#l' + my_id).html(level + '%').fadeIn( 400 ).delay( 800 ).fadeOut( 400 );
-});
 function updateProgress(percent, dev_id, dev_val){
     if(dev_val == "on") {
         $('#one' + dev_id).removeClass('glyphicon-dot-off').addClass('glyphicon-dot-on');
@@ -70,3 +12,63 @@ function updateProgress(percent, dev_id, dev_val){
         //$('#ps' + dev_id).val(dev_per);
     }
 }
+$(document).ready(function () {
+  $(".act").click(function() {
+    var dev_id = $(this).closest("form").attr('id');
+    var dev_val = $(this).val();
+    var dev_per = $('#ps' + dev_id).attr('value');
+    var data = '{"on":';
+    if(dev_val == "off") {
+      percent = 0;
+      data += 'false';
+    }else{
+      percent = dev_per;
+      data += 'true';
+      if(dev_per < 100) {
+        data += ', "bri":' + Math.round(2.55 * dev_per);
+      }
+    }
+    data += '}';
+    $.ajax({
+      type: 'POST',
+      dataType: 'json',
+      url: 'http://' + SN + ':' + port + '/api/c/lights/' + dev_id + '/state',
+      headers: {"X-HTTP-Method-Override": "PUT"},
+      data: data,
+      success : updateProgress(percent, dev_id, dev_val)
+    });
+    return false;
+  });
+  $(".upd").click(function() {
+    var dev_id = $(this).closest("form").attr('id');
+    var dev_val = $(this).val();
+    var dev_per = $('#ps' + dev_id).attr('value');
+    var data = '{"on":';
+    if(dev_val == "off") {
+      percent = 0;
+      data += 'false';
+    }else{
+      percent = dev_per;
+      data += 'true';
+      if(dev_per < 100) {
+        data += ', "bri":' + Math.round(2.55 * dev_per);
+      }
+    }
+    data += '}';
+    $.ajax({
+      type: 'POST',
+      dataType: 'json',
+      url: 'http://' + SN + ':' + port + '/api/c/lights/' + dev_id + '/bridgeupdatestate',
+      headers: {"X-HTTP-Method-Override": "PUT"},
+      data: data,
+      success : updateProgress(percent, dev_id, dev_val)
+    });
+    return false;
+  });
+  $('.sl').on("change", function() {
+      var my_id = $(this).closest("form").attr('id');
+      var level = $(this).val();
+      $('#ps' + my_id).val(level);
+      $('#l' + my_id).html(level + '%').fadeIn( 400 ).delay( 800 ).fadeOut( 400 );
+  });
+});
